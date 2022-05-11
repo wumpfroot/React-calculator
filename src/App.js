a7ef160a2d6edbd0ab5329cf6368bc4dde609865
wumpfroot <markuskojo@msn.com>
2022-05-11 15:06:24 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { evaluate } from "mathjs";
+import { useState } from "react";
 
 function App() {
+  const btnValue = ["7","8","9","C","4","5","6","*","1","2","3","/","+","0","-","="]
+  const [value, setValue] = useState("")
+  
+  const handleClick = (str) => { 
+  setValue(str === "=" ? evaluate(value)
+          : str === "C" ? ""
+          : value + str)
+        }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div className='App'>
+  <h1>react-calculator</h1>
+  <div className='Calculator'>
+    <div className='calculator-screen'>
+      <h1>{value}</h1>
     </div>
+    <div className='Buttons'>
+    {btnValue.map((button, index) => {
+    return (
+    <button onClick={() => handleClick(button)} key={index}>{button}</button>
+    )
+})}
+    </div>
+  </div>
+</div>
   );
 }
-
 export default App;
